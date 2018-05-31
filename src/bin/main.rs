5ba06cffbd80416ae64766e3202533b07973294e
unknown <inv2004@gmail.com>
2018-05-31 02:50:59 -0400

    update

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -8,13 +8,13 @@ fn main() {
     h8.insert(9, 2);
     h8.insert(9, 3);
     h8.insert(10, 4);
-    println!("{:?}", h8.get(8));
-    println!("{:?}", h8.get(11));
-    println!("{:?}", h8.get(9));
-    h8.update(9, 5);
-    println!("{:?}", h8.get(9));
-    *h8.get_mut_def(9, 10) += 1;
-    println!("{:?}", h8.get(9));
-    *h8.get_mut_def(19, 0) += 1;
-    println!("{:?}", h8.get(19));    
+    println!("{:?}", h8.get(&8));
+    println!("{:?}", h8.get(&11));
+    println!("{:?}", h8.get(&9));
+    h8.update(&9, 5);
+    println!("{:?}", h8.get(&9));
+    *h8.get_mut_def(&9, 10) += 1;
+    println!("{:?}", h8.get(&9));
+    *h8.get_mut_def(&19, 0) += 1;
+    println!("{:?}", h8.get(&19));    
 }
